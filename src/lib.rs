bc0a25ca1d0febf39ccf62ff8c578c71e45d25a9
siishsusu <byvladyslava@gmail.com>
2024-11-10 12:54:48 +0200

    added anyhow to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 use std::fs::{File};
 use std::path::Path;
 use std::*;
-use std::io::BufRead;
+use std::io::{BufRead, BufReader};
+use anyhow::{anyhow, Context};
 
-pub fn read_lines(filename: &str) -> Result<Vec<String>, io::Error> {
+pub fn read_lines(filename: &str) -> anyhow::Result<Vec<String>> {
     let path = Path::new(filename);
     if !path.exists() {
-        return Err(io::Error::new(io::ErrorKind::NotFound, "File not found"));
+        return Err(anyhow!("No file found: {:?}", path));
     }
 
-    let file = File::open(filename)?;
-    let reader = io::BufReader::new(file);
-    let lines = reader.lines().collect::<Result<Vec<String>, io::Error>>()?;
-
-    Ok(lines)
+    let file = File::open(filename)
+        .with_context(|| format!("Failed to open the file {}", filename))?;
+    let reader = BufReader::new(file);
+    let lines: Result<Vec<String>, io::Error> = reader.lines().collect();
+    lines.map_err(|e| anyhow::anyhow!("Failed to read the file: {}: {}", filename, e))
 }
