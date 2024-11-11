55585a30177ebaa36b09482eed7d12a5dfb240fc
siishsusu <byvladyslava@gmail.com>
2024-11-11 19:26:03 +0200

    implemented parse_to_struct

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ use std::path::Path;
 use std::*;
 use std::io::{BufRead, BufReader};
 use anyhow::{anyhow, Context};
+use pest::Parser;
+use pest_derive::Parser;
+use std::io::Write;
 
 pub fn read_lines(filename: &str) -> anyhow::Result<Vec<String>> {
     let path = Path::new(filename);
@@ -13,6 +16,182 @@ pub fn read_lines(filename: &str) -> anyhow::Result<Vec<String>> {
     let file = File::open(filename)
         .with_context(|| format!("Failed to open the file {}", filename))?;
     let reader = BufReader::new(file);
-    let lines: Result<Vec<String>, io::Error> = reader.lines().collect();
-    lines.map_err(|e| anyhow::anyhow!("Failed to read the file: {}: {}", filename, e))
+    let lines: Result<Vec<String>, std::io::Error> = reader.lines().collect();
+    lines.map_err(|e| anyhow!("Failed to read the file: {}: {}", filename, e))
+}
+
+#[derive(Parser)]
+#[grammar = "film.pest"]
+pub struct FilmParser;
+
+#[derive(Debug, Clone)]
+pub struct Film {
+    title: String,
+    year: u32,
+    director: String,
+    writer: String,
+    genre: Vec<String>,
+    stars: Vec<String>,
+    description: String,
+}
+
+impl Film {
+    pub fn new(title: String, year: u32, director: String, writer: String, genre: Vec<String>, stars: Vec<String>, description: String) -> Self {
+        Film {
+            title,
+            year,
+            director,
+            writer,
+            genre,
+            stars,
+            description,
+        }
+    }
+
+    fn parse_to_struct(pair: pest::iterators::Pair<Rule>) -> Option<Self> {
+        let mut title = String::new();
+        let mut year = 0;
+        let mut director = String::new();
+        let mut writer = String::new();
+        let mut genre = Vec::new();
+        let mut stars = Vec::new();
+        let mut description = String::new();
+
+        for inner_pair in pair.into_inner() {
+            match inner_pair.as_rule() {
+                Rule::film => {
+                    for inner_pair_1 in inner_pair.into_inner() {
+                        match inner_pair_1.as_rule() {
+                            Rule::Title => {
+                                title = inner_pair_1.into_inner()
+                                    .find_map(|inner_inner_pair| {
+                                        if let Rule::title_value = inner_inner_pair.as_rule() {
+                                            Some(inner_inner_pair.as_str().to_string())
+                                        } else {
+                                            None
+                                        }
+                                    })
+                                    .unwrap_or_else(|| String::new());
+                            }
+                            Rule::Year => {
+                                let year_str = inner_pair_1.into_inner().as_str();
+                                match year_str.parse::<u32>() {
+                                    Ok(parsed_year) => {
+                                        year = parsed_year;
+                                    }
+                                    Err(_) => {
+                                        eprintln!("Failed to parse year: {}", year_str);
+                                        year = 0;
+                                    }
+                                }
+                            }
+                            Rule::Director => {
+                                director = inner_pair_1.into_inner()
+                                    .find_map(|inner_inner_pair| {
+                                        if let Rule::director_value = inner_inner_pair.as_rule() {
+                                            Some(inner_inner_pair.as_str().to_string())
+                                        } else {
+                                            None
+                                        }
+                                    })
+                                    .unwrap_or_else(|| String::new());
+                            }
+                            Rule::Writer => {
+                                writer = inner_pair_1.into_inner()
+                                    .find_map(|inner_inner_pair| {
+                                        if let Rule::writer_value = inner_inner_pair.as_rule() {
+                                            Some(inner_inner_pair.as_str().to_string())
+                                        } else {
+                                            None
+                                        }
+                                    })
+                                    .unwrap_or_else(|| String::new());
+                            }
+                            Rule::Genre => {
+                                genre = inner_pair_1.into_inner()
+                                    .find_map(|inner_inner_pair| {
+                                        if let Rule::genre_list = inner_inner_pair.as_rule() {
+                                            Some(inner_inner_pair.into_inner()
+                                                .map(|g| g.as_str().trim_matches('"').to_string())
+                                                .flat_map(|g| g.split(',').map(|s| s.trim().to_string()).collect::<Vec<String>>())
+                                                .collect::<Vec<String>>())
+                                        } else {
+                                            None
+                                        }
+                                    })
+                                    .unwrap_or_else(|| Vec::new());
+                            },
+
+                            Rule::Stars => {
+                                stars = inner_pair_1.into_inner()
+                                    .find_map(|inner_inner_pair| {
+                                        if let Rule::stars_list = inner_inner_pair.as_rule() {
+                                            Some(inner_inner_pair.into_inner()
+                                                .map(|star| star.as_str().trim_matches('"').to_string())
+                                                .flat_map(|star| star.split(',').map(|s| s.trim().to_string()).collect::<Vec<String>>())
+                                                .collect::<Vec<String>>())
+                                        } else {
+                                            None
+                                        }
+                                    })
+                                    .unwrap_or_else(|| Vec::new());
+                            }
+                            Rule::Description => {
+                                description = inner_pair_1.into_inner()
+                                    .find_map(|inner_inner_pair| {
+                                        if let Rule::description_value = inner_inner_pair.as_rule() {
+                                            Some(inner_inner_pair.as_str().to_string())
+                                        } else {
+                                            None
+                                        }
+                                    })
+                                    .unwrap_or_else(|| String::new());
+                            }
+                            _ => {
+                                println!("Unknown rule inside film: {:?}", inner_pair_1.as_rule());
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    println!("Unknown rule: {:?}", inner_pair.as_rule());
+                }
+            }
+        }
+
+        Some(Self::new(
+            title,
+            year,
+            director,
+            writer,
+            genre,
+            stars,
+            description,
+        ))
+    }
+
+}
+
+pub fn parse_films(films: Vec<String>) -> anyhow::Result<Vec<Film>> {
+    let mut films_res = Vec::new();
+
+    for film in films {
+        let pairs = match FilmParser::parse(Rule::file, &film) {
+            Ok(pairs) => pairs,
+            Err(err) => {
+                eprintln!("Failed to parse line: {} with error: {}", film, err);
+                continue;
+            }
+        };
+
+        for pair in pairs {
+            if let Some(film) = Film::parse_to_struct(pair) {
+                films_res.push(film);
+            } else {
+                eprintln!("Failed to parse film: {}", film);
+            }
+        }
+    }
+
+    Ok(films_res)
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() -> anyhow::Result<()> {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 2 {
-        eprintln!("{}{}", "Error".red().bold(), ": No command provided. Use 'cargo run -- help' for usage information.");
+        eprintln!("{}: No command provided. Use 'cargo run -- help' for usage information.", "Error".red().bold());
         return Ok(());
     }
 
@@ -18,14 +18,13 @@ fn main() -> anyhow::Result<()> {
         }
         "parse" => {
             if args.len() <= 2 {
-                eprintln!("{}{}", "Error".red().bold(), ": No file path provided. You must specify path to the file with information.");
+                eprintln!("{}: No file path provided. You must specify path to the file with information.", "Error".red().bold());
                 return Ok(());
             }
             let filepath = &args[2];
             match read_lines(filepath) {
                 Ok(lines) => {
-                    println!("Successfully read {} lines from file '{}'.", lines.len().to_string().green(), filepath.green());
-                    println!("{:?}", lines.get(0));
+                    parse_films(lines)?;
                 }
                 Err(e) => eprintln!(
                     "{}'{}': {}",
@@ -35,18 +34,17 @@ fn main() -> anyhow::Result<()> {
                 ),
             }
         }
-        _ => eprintln!("{}{}", "Error".red().bold(), ": Invalid command. Use 'cargo run -- help' for usage information."),
+        _ => eprintln!("{}: Invalid command. Use 'cargo run -- help' for usage information.", "Error".red().bold()),
     }
     Ok(())
 }
 
-
 fn show_help(){
     println!("{}", "\nFilm Parser - A command-line tool for parsing film information from file.".green().bold().italic());
     println!("{}", "Commands:".green().bold());
-    println!("{}  - {}", "\tparse <filename>".italic(), "Parse the specified file and display its content.");
-    println!("{}              - {}", "\thelp".italic(), "Show this help information.");
-    println!("{}           - {}", "\tcredits".italic(), "Show credits information.");
+    println!("{}  - Parse the specified file and display its content.", "\tparse <filename>".italic());
+    println!("{}              - Show this help information.", "\thelp".italic());
+    println!("{}           - Show credits information.", "\tcredits".italic());
     println!("{}", "\nExample usage:".green().bold());
     println!("{}", "\tcargo run -- parse data/film_info.txt".italic());
     println!("{}", "\tcargo run -- help".italic());
@@ -55,6 +53,6 @@ fn show_help(){
 
 fn show_credits() {
     println!("{}", "Film Parser v1.0".italic());
-    println!("{} {}", "Developed by", "Rudas Vladyslava".bold());
+    println!("Developed by {}", "Rudas Vladyslava".bold());
     println!("{}", "Thanks for using the Film Parser CLI!".italic().yellow());
 }
